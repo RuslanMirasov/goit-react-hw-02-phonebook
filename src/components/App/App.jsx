85931a9e606034fccc80b9d5b387488798c1c57e
RuslanMirasov <info@mirasov.dev>
2023-09-06 00:43:50 +0200

    Initial commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import css from './App.module.css';
+import { Section } from 'components/Section/Section';
+import { Notification } from 'components/Notification/Notification';
+
+export class App extends React.Component{
+
+  state = {
+    contacts: [],
+    filter: '',
+    name: '',
+    number: ''
+  }
+
+  render() {
+    return (
+      <main className={css.main}>
+        <h1 hidden>React homework 2</h1>
+
+        <Section title='Phonebook'>
+          <h2>Phonebook</h2>
+        </Section>
+
+        <Section title='Contacts'>
+          <Notification message='There is no contacts in phonebook!'></Notification>
+        </Section>       
+
+      </main>
+    );
+  }
+
+
+
+
+
+};
